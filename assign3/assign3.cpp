4683b9feb7e876408e01ba6304816c9478fa84a8
Ryan Zubery <ryan.zubery@gmail.com>
2019-11-29 10:07:07 -0700

    working intersections, phong shading for spheres

--- assign3/assign3.cpp
+++ assign3/assign3.cpp
@@ -13,6 +13,8 @@ Name: Ryan Zubery
 #include <string.h>
 #include <math.h>
 #include <iostream>
+#include <algorithm>
+#include <limits>
 
 #define MAX_TRIANGLES 2000
 #define MAX_SPHERES 10
@@ -71,6 +73,14 @@ typedef struct _Sphere
   double radius;
 } Sphere;
 
+struct Hit
+{
+  bool found; 
+  float t; 
+  struct _Triangle tri; 
+  struct _Sphere sph; 
+}; 
+
 typedef struct _Light
 {
   double position[3];
@@ -98,8 +108,14 @@ void plot_pixel(int x,int y,unsigned char r,unsigned char g,unsigned char b);
 
 void create_frustum();
 struct Vector3 lerpRay(int x, int y);
-void triangle_intersection(struct Vector3 direction, struct Color color);
-void sphere_intersection(struct Vector3 direction, struct Color& color);
+float dotProduct(struct Vector3 v1, struct Vector3 v2);
+struct Vector3 crossProduct(struct Vector3 v1, struct Vector3 v2);
+struct Vector3 segment(struct Vector3 point1, struct Vector3 point2);
+struct Vector3 normalize(struct Vector3 original); 
+void triangle_intersection(struct Vector3 direction, struct Hit& hit);
+void sphere_intersection(struct Vector3 direction, struct Hit& hit);
+bool pointInTriangle(struct Vector3 point, struct _Triangle tri); 
+struct Color PhongSphere(struct Vector3 point, struct _Sphere sph); 
 
 //MODIFY THIS FUNCTION
 void draw_scene()
@@ -118,14 +134,37 @@ void draw_scene()
       struct Vector3 direction; 
       struct Color color;
 
+      struct Hit triHit; 
+      triHit.found = false; 
+      triHit.t = std::numeric_limits<float>::max(); 
+      struct Hit sphHit; 
+      sphHit.found = false; 
+      sphHit.t = std::numeric_limits<float>::max(); 
+
       direction = lerpRay(x, y);
 
       color.r = 255;
       color.g = 255;
       color.b = 255;
 
-      triangle_intersection(direction, color);
-      sphere_intersection(direction, color); 
+      triangle_intersection(direction, triHit);
+      sphere_intersection(direction, sphHit); 
+
+      if(triHit.found && triHit.t < sphHit.t)
+      {
+        color.r = 0; 
+        color.g = 0; 
+        color.b = 0; 
+      }
+      else if(sphHit.found && sphHit.t < triHit.t)
+      {
+        struct Vector3 point; 
+        point.x = camera.x + (direction.x * sphHit.t); 
+        point.y = camera.y + (direction.y * sphHit.t); 
+        point.z = camera.z + (direction.z * sphHit.t); 
+
+        color = PhongSphere(point, sphHit.sph); 
+      }
 
       plot_pixel(x,y,color.r,color.g,color.b);
     }
@@ -141,7 +180,8 @@ void create_frustum()
   camera.y = 0.0f;
   camera.z = 0.0f;
 
-  float aspect = WIDTH/HEIGHT;
+  float aspect = ((float)WIDTH) / ((float)HEIGHT);
+
   float halfFOV = fov / 2.0f;
   float halfFOVRad = halfFOV * (M_PI / 180.0f);
   float tanHalfFOVRad = tan(halfFOVRad);
@@ -193,15 +233,148 @@ struct Vector3 lerpRay(int x, int y)
   return result; 
 }
 
-void triangle_intersection(struct Vector3 direction, struct Color color)
+float dotProduct(struct Vector3 v1, struct Vector3 v2)
+{
+  float result = (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+  return result; 
+}
+
+struct Vector3 crossProduct(struct Vector3 v1, struct Vector3 v2)
+{
+  struct Vector3 result; 
+
+  result.x = (v1.y * v2.z) - (v1.z * v2.y); 
+  result.y = (v1.z * v2.x) - (v1.x * v2.z);
+  result.z = (v1.x * v2.y) - (v1.y * v2.x); 
+
+  return result; 
+}
+
+struct Vector3 segment(struct Vector3 point1, struct Vector3 point2)
+{
+  struct Vector3 result; 
+
+  result.x = point2.x - point1.x; 
+  result.y = point2.y - point1.y; 
+  result.z = point2.z - point1.z; 
+
+  return result; 
+}
+
+struct Vector3 normalize(struct Vector3 original)
+{
+  float length = sqrt(dotProduct(original, original)); 
+  
+  struct Vector3 result; 
+  result.x = original.x / length; 
+  result.y = original.y / length; 
+  result.z = original.z / length; 
+
+  return result; 
+}
+
+void triangle_intersection(struct Vector3 direction, struct Hit& hit)
 {
   for(int i = 0; i < num_triangles; i++)
   {
+    struct _Triangle current = triangles[i];
 
+    struct Vector3 normal0; 
+    normal0.x = current.v[0].normal[0]; 
+    normal0.y = current.v[0].normal[1]; 
+    normal0.z = current.v[0].normal[2]; 
+    
+    struct Vector3 vertex0;
+    vertex0.x = current.v[0].position[0];
+    vertex0.y = current.v[0].position[1];
+    vertex0.z = current.v[0].position[2];
+
+    float t; 
+    float top; 
+    float bottom = dotProduct(normal0, direction);
+
+    if(!(fabs(0.0f - bottom) < 0.005f))
+    {
+      float d = dotProduct(normal0, vertex0); 
+      top = (dotProduct(normal0, camera) + d); 
+
+      t = top / bottom; 
+
+      if(t >= 0)
+      {
+        struct Vector3 intersection; 
+        intersection.x = camera.x + (direction.x * t); 
+        intersection.y = camera.y + (direction.y * t); 
+        intersection.z = camera.z + (direction.z * t); 
+
+        //std::cout << intersection.x << ", " << intersection.y << ", " << intersection.z << std::endl; 
+
+        bool inside = pointInTriangle(intersection, current); 
+
+        if(inside)
+        {
+          if(hit.found == false)
+          {
+            hit.found = true; 
+            hit.t = t;
+            hit.tri = current; 
+          }
+          else if(t < hit.t)
+          {
+            hit.t = t; 
+            hit.tri = current; 
+          }
+        }
+      }
+    }
   }
 }
 
-void sphere_intersection(struct Vector3 direction, struct Color& color)
+bool pointInTriangle(struct Vector3 point, struct _Triangle tri)
+{
+  struct Vector3 vertA; 
+  vertA.x = tri.v[0].position[0]; 
+  vertA.y = tri.v[0].position[1]; 
+  vertA.z = tri.v[0].position[2]; 
+
+  struct Vector3 vertB; 
+  vertB.x = tri.v[1].position[0]; 
+  vertB.y = tri.v[1].position[1]; 
+  vertB.z = tri.v[1].position[2]; 
+
+  struct Vector3 vertC; 
+  vertC.x = tri.v[2].position[0]; 
+  vertC.y = tri.v[2].position[1]; 
+  vertC.z = tri.v[2].position[2]; 
+
+  struct Vector3 u = segment(vertA, vertB); 
+  struct Vector3 v = segment(vertA, vertC); 
+  struct Vector3 w = segment(vertA, point); 
+
+  struct Vector3 vCrossW = crossProduct(v, w); 
+  struct Vector3 vCrossU = crossProduct(v, u); 
+
+  if(dotProduct(vCrossW, vCrossU) < 0.0f)
+  {
+    return false; 
+  }
+
+  struct Vector3 uCrossW = crossProduct(u, w); 
+  struct Vector3 uCrossV = crossProduct(u, v); 
+
+  if(dotProduct(uCrossW, uCrossV) < 0.0f)
+  {
+    return false; 
+  }
+
+  float divideBy = sqrt(dotProduct(uCrossV, uCrossV)); 
+  float r = sqrt(dotProduct(vCrossW, vCrossW)) / divideBy; 
+  float t = sqrt(dotProduct(uCrossW, uCrossW)) / divideBy; 
+
+  return (r + t <= 1); 
+}
+
+void sphere_intersection(struct Vector3 direction, struct Hit& hit)
 {
   for(int i = 0; i < num_spheres; i++)
   {
@@ -217,17 +390,131 @@ void sphere_intersection(struct Vector3 direction, struct Color& color)
     float c = pow(camera.x - center.x, 2.0f) + pow(camera.y - center.y, 2.0f) + pow(camera.z - center.z, 2.0f) - pow(current.radius, 2.0f);
 
     float delta = pow(b, 2.0f) - (4 * a * c); 
+    float t0; 
+    float t1;
 
     if(delta >= 0)
     {
-      color.r = 0;
-      color.g = 0;
-      color.b = 0;
+      t0 = ((-1 * b) + (sqrt(delta))) / 2.0f;
+      t1 = ((-1 * b) - (sqrt(delta))) / 2.0f;
+
+      if(hit.found == false)
+      {
+        hit.found = true; 
+        hit.t = std::min(t0, t1);
+        hit.sph = current; 
+      }
+      else
+      {
+        float tmin = std::min(t0, t1); 
+        if(tmin < hit.t)
+        {
+          hit.t = tmin; 
+          hit.sph = current; 
+        }
+      }
+    }
+  }
+}
+
+struct Color PhongSphere(struct Vector3 point, struct _Sphere sph)
+{
+  float kSpecularR = sph.color_specular[0];
+  float kDiffuseR = sph.color_diffuse[0]; 
+  float kAmbientR = ambient_light[0]; 
+
+  float kSpecularG = sph.color_specular[1];
+  float kDiffuseG = sph.color_diffuse[1]; 
+  float kAmbientG = ambient_light[1];
+
+  float kSpecularB = sph.color_specular[2];
+  float kDiffuseB = sph.color_diffuse[2]; 
+  float kAmbientB = ambient_light[2];
+
+  float shine = sph.shininess; 
+
+  float phongR = kAmbientR; 
+  float phongG = kAmbientG; 
+  float phongB = kAmbientB; 
+
+  struct Vector3 sphCenter; 
+  sphCenter.x = sph.position[0]; 
+  sphCenter.y = sph.position[1]; 
+  sphCenter.z = sph.position[2]; 
+
+  for(int i = 0; i < num_lights; i++)
+  {
+    struct _Light current = lights[i];
+
+    struct Vector3 lPoint; 
+    lPoint.x = current.position[0]; 
+    lPoint.y = current.position[1]; 
+    lPoint.z = current.position[2]; 
+
+    float currR = current.color[0]; 
+    float currG = current.color[1]; 
+    float currB = current.color[2]; 
+
+    struct Vector3 L = segment(point, lPoint); 
+    L = normalize(L);  
+
+    struct Vector3 normal = segment(sphCenter, point); 
+    normal = normalize(normal); 
+
+    struct Vector3 REnd; 
+    REnd.x = 2 * dotProduct(normal, L) * normal.x; 
+    REnd.y = 2 * dotProduct(normal, L) * normal.y; 
+    REnd.z = 2 * dotProduct(normal, L) * normal.z; 
+
+    struct Vector3 R = segment(L, REnd); 
+    R = normalize(R); 
+
+    struct Vector3 V = segment(point, camera); 
+    V = normalize(V); 
+
+    float lDotN = dotProduct(L, normal); 
+    if(lDotN < 0.0f)
+    {
+      lDotN = 0.0f; 
     }
 
-    float t0; 
-    float t1;
+    float rDotV = dotProduct(R, V);
+    if(rDotV < 0.0f)
+    {
+      rDotV = 0.0f; 
+    }
+
+    currR *= ((kDiffuseR * lDotN) + (kSpecularR * pow(rDotV, shine))); 
+
+    currG *= ((kDiffuseG * lDotN) + (kSpecularG * pow(rDotV, shine))); 
+
+    currB *= ((kDiffuseB * lDotN) + (kSpecularB * pow(rDotV, shine))); 
+
+    phongR += currR; 
+    phongG += currG; 
+    phongB += currB; 
   }
+
+  if(phongR > 1.0f)
+  {
+    phongR = 1.0f; 
+  }
+  else if(phongG > 1.0f)
+  {
+    phongG = 1.0f; 
+  }
+  else if(phongB > 1.0f)
+  {
+    phongB = 1.0f; 
+  }
+
+  struct Color result;
+
+  result.r = (int)(255 * phongR); 
+  result.g = (int)(255 * phongR); 
+  result.b = (int)(255 * phongR);
+
+  return result; 
 }
 
 void plot_pixel_display(int x,int y,unsigned char r,unsigned char g,unsigned char b)
